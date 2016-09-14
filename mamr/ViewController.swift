0889eeac54fe294ee73d043d0c464c1463e0f539
Fabian <fabian@dahmakan.com>
2016-09-14 17:33:43 +0200

    Initial Commit

---
+++ mamr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mamr
+//
+//  Created by Fabian Wetekamp on 14/09/2016.
+//  Copyright © 2016 Fabian Wetekamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ mamrTests/mamrTests.swift
@@ -0,0 +1,36 @@
+//
+//  mamrTests.swift
+//  mamrTests
+//
+//  Created by Fabian Wetekamp on 14/09/2016.
+//  Copyright © 2016 Fabian Wetekamp. All rights reserved.
+//
+
+import XCTest
+@testable import mamr
+
+class mamrTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
